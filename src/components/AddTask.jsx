f11a802088aae94907f22a3c9f054571b9c986da
Felipe Cavalcanti Caminha <felipe.ccaminha@liga.pe.gov.br>
2025-11-06 15:27:18 -0300

    gerenciador de tarefas com react feito

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,3 +1,15 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({onAddTaskSubmit}) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    return (
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite o titulo da tarefa" className="border border-slate-400 outline-slate-400 px-4 pt-2 rounded-md" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" placeholder="Digite a descrição da tarefa" className="border border-slate-400 outline-slate-400 px-4 pt-2 rounded-md" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <button className="bg-slate-400 text-white p-2 rounded-md hover:bg-slate-500" onClick={() => onAddTaskSubmit(title, description)}>Adicionar Tarefa</button>
+        </div>
+    );
 }
 export default AddTask;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,22 @@
-import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function Tasks(props) { 
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) { 
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set('title', task.title);
+        query.set('description', task.description);
+        navigate(`/task/${task.id}?${query.toString()}`);
+    }
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow-md mt-6">
-            {props.tasks.map((task) => (
+            {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
-                <button onClick={() => props.onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted ? 'line-through' : ''}`}>{task.title}</button>
-                <button><ChevronRightIcon className="bg-slate-400 p-2 rounded-md text-white" /></button>
-                <button onClick={() => props.onDeleteTaskClick(task.id)}><TrashIcon className="bg-slate-400 p-2 rounded-md text-white" /></button>
+                <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted ? 'line-through' : ''}`}>{task.title}</button>
+                <button onClick={() => onSeeDetailsClick(task)}><ChevronRightIcon className="bg-slate-400 p-2 rounded-md text-white"  /></button>
+                <button onClick={() => onDeleteTaskClick(task.id)}><TrashIcon className="bg-slate-400 p-2 rounded-md text-white" /></button>
                 </li>
             ))}
         </ul>
